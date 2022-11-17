9aa449af5a308c11732d4817d677301a0cc3d69e
AgathePons <ponsagathe37@gmail.com>
2022-11-17 18:37:13 +0100

    init project + add first components + styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GlobalContainerComponent } from './components/global-container/global-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    GlobalContainerComponent
   ],
   imports: [
     BrowserModule
